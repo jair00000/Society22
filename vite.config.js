e4a1aff494bb512c89e8a29c36ae2609aab8b08c
Jan Mark Pereda <peredajanmark@gmail.com>
2026-04-25 15:55:31 +0800

    Enhance Cookie Policy, FAQs, and Privacy Policy pages with detailed information and improved layout

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/build/',
+
+  build: {
+    outDir: path.resolve(__dirname, '../backend/public/build'),
+    emptyOutDir: true,
+  },
 })
